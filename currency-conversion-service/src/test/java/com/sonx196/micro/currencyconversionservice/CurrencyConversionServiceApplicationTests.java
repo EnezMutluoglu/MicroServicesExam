f0a8300c9004be36eab91e4ac5b6bf5313e5474d
Enez Mutluoğlu <44931036+EnezMutluoglu@users.noreply.github.com>
2021-11-13 18:41:20 +0300

    currency-conversion
    veriler exchange üzerinden proxy ile yönlendirildi

---
+++ currency-conversion-service/src/test/java/com/sonx196/micro/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonx196.micro.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
